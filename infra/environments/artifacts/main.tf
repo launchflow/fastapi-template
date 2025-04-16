9acf9013b95ecabc10d729adc8107882bdc590a9
Josh <josh@launchflow.com>
2025-04-16 16:43:23 -0700

    infra.new code version 0.0.3

---
+++ infra/environments/artifacts/main.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  backend "gcs" {
+    bucket = "infra-new-state"
+    prefix = "tanke/v3/python-app/artifacts"
+  }
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "launchflow-services-dev"
+  region  = "us-west1"
+}
+
+resource "google_artifact_registry_repository" "app" {
+  location      = "us-west1"
+  repository_id = "app"
+  description   = "Docker repository for Python application"
+  format        = "DOCKER"
+}
